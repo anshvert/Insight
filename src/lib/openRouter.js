7905745c059e98df9e0a4ada3991550bbd43237c
ansh <anshtyagi2222@gmail.com>
2025-02-26 17:26:25 +0530

    api key updated

--- src/lib/openRouter.js
+++ src/lib/openRouter.js
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
-const apiKey = "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"
+const apiKey = "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"
 
 export const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
@@ -12,17 +12,17 @@ export const openai = new OpenAI({
     dangerouslyAllowBrowser: true,
 })
 
-// async function main() {
-//     const completion = await openai.chat.completions.create({
-//         model: 'openai/gpt-3.5-turbo',
-//         messages: [
-//             {
-//                 role: 'user',
-//                 content: 'What is the meaning of life?',
-//             },
-//         ],
-//     });
-//     console.log(completion.choices[0].message);
-// }
-//
-// main()
+async function main() {
+    const completion = await openai.chat.completions.create({
+        model: 'openai/gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'user',
+                content: 'What is the meaning of life?',
+            },
+        ],
+    });
+    console.log(completion.choices[0].message);
+}
+
+main()
